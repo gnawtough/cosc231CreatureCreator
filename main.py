6dac07143336c47cc19823d2dd27fd4a69d03f76
gnawtough <avkoller01@hotmail.com>
2024-05-03 12:50:53 -0500

    now shows alternating facts

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ def retreive_facts(firstAnimal, secondAnimal):
 
     firstAnimal = parse_json(firstAnimal, data)
     secondAnimal = parse_json(secondAnimal, data)
-    print(firstAnimal, secondAnimal)
+    #print(firstAnimal['habitat'], secondAnimal['location'])
 
+    keys = list(firstAnimal.keys())
+    for i in range(0, 9):
+        if i % 2 == 0:
+            print(f"{keys[i]}: {firstAnimal[keys[i]]}")
+        else:
+            print(f"{keys[i]}: {secondAnimal[keys[i]]}")
 def parse_json(animal_name, data):
     for animal in data:
         if animal_name.lower() in animal['title'].lower():
             print(animal['title'])
             return animal['attributes']
-
     return None
 def main():
     animalInput1 = input("Pick the name of the first animal you want:")
